eb3483450b3a008c056544fe7d23b7ab068aff35
Sergey Volkov <sergey.volkov@coverwallet.com>
2019-12-13 16:34:42 +0300

    Update Dockerfile; Add build-container.sh

---
+++ build-container.sh
@@ -0,0 +1,9 @@
+# creates a nodejs app bundle
+set -x
+rm -rf /tmp/rocketchat-build
+meteor build --directory /tmp/rocketchat-build
+
+# create docker image
+cp .docker/Dockerfile /tmp/rocketchat-build
+cd /tmp/rocketchat-build
+docker build -t rocketchat-dc .
