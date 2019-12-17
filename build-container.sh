eb4b0133290ddeef9b29479f5f544f844d7b0132
Matt LaPaglia <mlapaglia@gmail.com>
2019-12-17 14:03:59 -0500

    or this

--- build-container.sh
+++ build-container.sh
@@ -1,6 +1,9 @@
 # creates a nodejs app bundle
 set -x
-meteor build
+rm -rf ./tmp
+meteor build --directory ./tmp
 
 # create docker image
+cp .docker/Dockerfile ./tmp
+cd ./tmp
 docker build -t rocketchat-dc .
