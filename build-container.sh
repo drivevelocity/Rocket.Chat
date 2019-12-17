25903ee32d268972ddb0d4a7e382554c97e54df1
Matt LaPaglia <mlapaglia@gmail.com>
2019-12-17 13:54:26 -0500

    sub folder

--- build-container.sh
+++ build-container.sh
@@ -1,9 +1,9 @@
 # creates a nodejs app bundle
 set -x
-rm -rf /tmp/rocketchat-build
-meteor build --directory /tmp/rocketchat-build
+rm -rf ./tmp/rocketchat-build
+meteor build --directory ./tmp/rocketchat-build
 
 # create docker image
-cp .docker/Dockerfile /tmp/rocketchat-build
-cd /tmp/rocketchat-build
+cp .docker/Dockerfile ./tmp/rocketchat-build
+cd ./tmp/rocketchat-build
 docker build -t rocketchat-dc .
